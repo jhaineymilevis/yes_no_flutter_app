bbcde68cd8023b7ae73712399baef1a96e1eda80
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-13 19:47:36 -0500

    manage focus on the text field

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,31 +5,41 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-
+    final FocusNode focusNode = FocusNode();
+    // Create a TextEditingController to handle text input.
+    final textEditingController = TextEditingController();
+    // Create a border to give the input field a border style.
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a ??',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
-          print("valor de la caja de texto");
+          final textValue = textEditingController.value.text;
+          print("boton: $textValue");
+          textEditingController.clear(); //con esto limpio el campo de texto
         },
       ),
     );
 
     return TextFormField(
+      focusNode: focusNode,
+      controller: textEditingController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print("submit: $value");
+        textEditingController.clear(); //con esto limpio el campo de texto
+        focusNode
+            .requestFocus(); //recupero el foco en el campo despues dle submit
       },
-      onChanged: (value) {
-        print("changed: $value");
+      onTapOutside: (event) {
+        focusNode.unfocus(); //elimina el foco del campo cuando clico por fuera
       },
     );
   }
